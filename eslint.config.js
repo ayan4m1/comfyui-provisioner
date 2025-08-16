753653fbeb8464dca6b132a653a56b4a18da2974
ayan4m1 <andrew@bulletlogic.com>
2025-08-16 13:26:31 -0400

    fix: clean up eslint imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
 import eslint from '@eslint/js';
 import { config, configs } from 'typescript-eslint';
-import eslintPluginImportX from 'eslint-plugin-import-x';
+import { flatConfigs as importConfigs } from 'eslint-plugin-import-x';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default config(
   eslint.configs.recommended,
   ...configs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  {
-    rules: {
-      'no-control-regex': 'off'
-    }
-  },
+  importConfigs.recommended,
+  importConfigs.typescript,
   eslintPluginPrettier
 );
